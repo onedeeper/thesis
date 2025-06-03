fixes bug weighting being used incorrectly in fine tune ssl

eeg-graph-learning/eeglearn/config.py
@@ -18,7 +18,7 @@ class Config:
     """
 
     # Development settings
-    experiment_name = "tuur_vanilla"
+    experiment_name = "tuur_finetuned"
     optuna :bool = False
     load_data_split_from  = "turr_all_data_train_test_valid_split.pt"
     # Reproducibility settings
@@ -27,10 +27,10 @@ class Config:
 
     # Training hyperparameters
     epochs = 100
-    batch_size = 128
-    lr = 0.0029199817361526665
-    weight_decay = 3.455510901178435e-05
-    drop_rate = 0.15154331681957503
+    batch_size = 256
+    lr = 0.0016898579982266685
+    weight_decay = 0.0005931173538055033
+    drop_rate = 0.1360323026782416
     stop_at = 10
 
     # Data selection
@@ -49,15 +49,15 @@ class Config:
 
     # Model architecture parameters
     gcn_out_size = 64
-    linear_size =  1024
+    linear_size =  512
     K = 1
     
     # for fine tuning.
-    pretrained_weights_path = "/Users/udeshhabaraduwa/thesis _local/thesis/eeg-graph-learning/data/weights/self_supervised/test_self_supervised_2222_best_model_val_loss_7.2698_epoch_0.pt"
-    pretrained_gcn_out_size = 32  # Example: Must match your actual SSL model
-    pretrained_k = 3              # Example: Must match your actual SSL model
-    pretrained_linear_size = 256  # Example: Must match your actual SSL model's HF/HS linear_size
-    pretrained_drop_rate = 0.10027792571799715    # Example: Must match your actual SSL model's HF/HS drop_rate
+    pretrained_weights_path = "/mnt/disk2/thesis/eeg-graph-learning/data/weights/self_supervised/tuur_data/tuur_data_self_supervised_best_model_val_loss_1.6647_epoch_25.pt"
+    pretrained_gcn_out_size = 64  # Example: Must match your actual SSL model
+    pretrained_k = 1              # Example: Must match your actual SSL model
+    pretrained_linear_size = 512  # Example: Must match your actual SSL model's HF/HS linear_size
+    pretrained_drop_rate = 0.20306923446428116    # Example: Must match your actual SSL model's HF/HS drop_rate
     
     num_workers = 8
     device = torch.device('cuda' if torch.cuda.is_available() else 'mps'\

eeg-graph-learning/eeglearn/models/train_finetune_from_ssl.py
@@ -113,14 +113,14 @@ def train_classification():
     # Config
 
     device = Config.device
-    batch_size = Config.batch_size
+    batch_size = Config.batch_size # <-- fine tuned
     epochs = Config.epochs
     stop_at = Config.stop_at
-    
-    lr = Config.lr 
-    weight_decay = Config.weight_decay
-    hc_drop_rate = Config.drop_rate        
-    hc_linear_size = Config.linear_size 
+     
+    lr = Config.lr  # <-- fine tuned
+    weight_decay = Config.weight_decay # <-- fine tuned
+    hc_drop_rate = Config.drop_rate    # <-- fine tuned    
+    hc_linear_size = Config.linear_size # <-- fine tuned
     pretrained_weights_path = Config.pretrained_weights_path
     model_weights_dir = Config.model_weights_dir / "fine_tune"
     model_metrics_dir = Config.metrics_dir / "fine_tune"
@@ -219,6 +219,8 @@ def train_classification():
 
     # Training setup
     criterion = nn.CrossEntropyLoss(weight =rescaled_class_weights).to(device)
+    if not Config.use_class_weighting:
+        criterion = nn.CrossEntropyLoss().to(device)
     optimizer = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, mode='min', factor=0.1, patience=4, threshold=0.0001,