fixes bug with class weighting being used for already undersampled data

eeg-graph-learning/eeglearn/models/train_jointly.py
@@ -154,7 +154,7 @@ def train() -> float:
     ).to(device)
     
     criterion_original = nn.CrossEntropyLoss(weight=rescaled_class_weights).to(device)
-    if Config.use_sampler_for_data_loading:
+    if not Config.use_class_weighting:
         criterion_original = nn.CrossEntropyLoss().to(device)
 
     criterion_permuted = nn.CrossEntropyLoss().to(device)